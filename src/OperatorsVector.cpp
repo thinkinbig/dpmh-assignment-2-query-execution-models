dd2a173acf16a1ac7a591465af133b2ca674e107
mike_georg <michalis.georgoulakis@tum.de>
2025-05-07 15:25:55 +0200

    Initial commit

---
+++ src/OperatorsVector.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file
+ *
+ * Implementation of vector-at-a-time execution for relational operators.
+ *
+ */
+
+/* TODO: Add openVec(), nextVec() and closeVec() implementations for vector-at-a-time processing */
+
+#include "Operators.h"
